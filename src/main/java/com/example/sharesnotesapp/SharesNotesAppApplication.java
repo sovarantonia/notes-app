7dd4b36164342cb2873bf1d45538b31f5d254192
sovarantonia <sovarantonia@yahoo.com>
2024-04-25 16:05:13 +0300

    Initial commit

---
+++ src/main/java/com/example/sharesnotesapp/SharesNotesAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.sharesnotesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharesNotesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharesNotesAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sharesnotesapp/SharesNotesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sharesnotesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharesNotesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
